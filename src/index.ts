fa97e1cb7a32918eee1a06aff265f41171f4684e
farisachugthai <farischugthai@gmail.com>
2020-08-23 10:38:12 -0400

    More REPL based code

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,68 @@
-#!/usr/bin/env node
-import '*' as fs from 'fs';
-let repl = require('repl')
-let readline = require('readline')
-// let path = require('path')
-let process = require('process')
-let vm = require('vm')
+#!/usr/bin/env tsc
+// I'm glad it still compiles with a shebang
 
-let term = console.Console(process.stdout)
+type Todo = Readonly<{
+  /*
+   * Readonly Types:
+   * To prevent a function from modifying it's input, you can use the readonly
+   * keyword in TypeScript. However, rather than modifying every property, let's
+   * modify the entire type.
+   * This is referred to as a mapped type. Mapped types are functions that
+   * take a TypeScript type that outputs a type.
+   * There are many built-in mapped types (like Required<...>, Partial<...>
+   * You can also create your own Mapped Types.
+   */
+  id: number
+  text: string
+  done: boolean
+}>
 
-// https://nodejs.org/api/repl.html#repl_recoverable_errors
-// As a user is typing input into the REPL prompt, pressing the <enter> key
-// will send the current line of input to the eval function. In order to support
-// multi-line input, the eval function can return an instance of repl.Recoverable
-// to the provided callback function:
-function myEval(cmd, context, filename, callback) {
-  let result;
-  try {
-    result = vm.runInThisContext(cmd);
-  } catch (e) {
-    if (isRecoverableError(e)) {
-      return callback(new repl.Recoverable(e));
-    }
+type CompletedTodo = Readonly<{
+  /*
+   * In TypeScript, you can use exact values (like true or false) when specifying
+   * a type. These are called literal types.
+   */
+  id: number
+  text: string
+  done: true
+}>
+
+function toggleTodo(todo: Todo): Todo {
+  // The input to toggleTodo must be Todo as shown by the `: Todo` after the todo.
+  // The return type of toggleTodo() must also be Todo.
+  // We do this by adding : Todo after the parameter list.
+  return {
+    id: todo.id,
+    text: todo.text,
+    done: !todo.done
   }
-  callback(null, result);
 }
 
-/*
- * How does jsdoc work?
- */
-function isRecoverableError(error) {
-  if (error.name === 'SyntaxError') {
-    return /^(Unexpected end of input|Unexpected token)/.test(error.message);
-  }
-  return false;
+const foo: Todo = {
+  id: 1,
+  text: '...',
+  done: true
 }
 
+const result = toggleTodo({
+  id: 1,
+  text: '...',
+  done: true
+})
 
-// Add some listeners
-process.on('exit', (code) => {
-  console.log(`About to exit with code: ${code}`);
-});
+console.log('Expected:')
+console.log(`{ id: 1, text: '...', done: false }`)
+console.log('Actual:')
+console.log(result)
 
-// The 'warning' event is emitted whenever Node.js emits a process warning.
-
-// A process warning is similar to an error in that it describes
-// exceptional conditions that are being brought to the user's attention.
-// However, warnings are not part of the normal Node.js and JavaScript
-// error handling flow. Node.js can emit warnings whenever it detects bad
-// coding practices that could lead to sub-optimal application
-// performance, bugs, or security vulnerabilities.
-
-process.on('warning', (warning) => {
-  console.warn(warning.name);    // Print the warning name
-  console.warn(warning.message); // Print the warning message
-  console.warn(warning.stack);   // Print the stack trace
-});
-
-// A common use case for readline is to consume an input file one line at
-// a time. The easiest way to do so is leveraging the fs.ReadStream API as
-// well as a for await...of loop
-//
-async function processLineByLine() {
-  const fileStream = fs.createReadStream('input.txt');
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
-
-  for await (const line of rl) {
-    // Each line in input.txt will be successively available here as `line`.
-    console.log(`Line from file: ${line}`);
-  }
+// Takes an array of todo items and returns a new array of todos where
+// done is all true. Also let's not modify the array of Todos and simply
+// return a new one so make that input of todos readonly
+function completeAll(
+  todos: readonly Todo[]
+): CompletedTodo[] {
+  return todos.map(todo => ({
+    ...todo,
+    done: true
+  }))
 }
--- src/learn.js
+++ src/learn.js
@@ -1,31 +1,41 @@
 #!/usr/bin/env node
-// let fs = require('fs')
+
+const fs = require('fs')
 let repl = require('repl')
-let readline = require('readline')
+const readline = require('readline')
 // let path = require('path')
 let process = require('process')
 let vm = require('vm')
+const os = require('os');
 
 let term = console.Console(process.stdout)
 
-// https://nodejs.org/api/repl.html#repl_recoverable_errors
-// As a user is typing input into the REPL prompt, pressing the <enter> key
-// will send the current line of input to the eval function. In order to support
-// multi-line input, the eval function can return an instance of repl.Recoverable
-// to the provided callback function:
+// Similar to import builtins
+const builtins = require('module').builtinModules;
 
+// https://nodejs.org/dist/latest-v12.x/docs/api/readline.html#readline_readline_createinterface_options
+// The readline.createInterface() method creates a new readline.Interface instance.
+// Example of template string.
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  historySize: 10000,
+  terminal: true,
+  prompt: `${process.cwd()}: >`,
+  removeHistoryDuplicates: true,
+});
 
-function myEval(cmd, context, filename, callback) {
-  let result;
-  try {
-    result = vm.runInThisContext(cmd);
-  } catch (e) {
-    if (isRecoverableError(e)) {
-      return callback(new repl.Recoverable(e));
-    }
-  }
-  callback(null, result);
-}
+rl.on('line', (line) => {
+  console.log(`Received: ${line}`);
+});
+
+// https://nodejs.org/dist/latest-v12.x/docs/api/readline.html#readline_event_sigint
+rl.on('SIGINT', () => {
+  rl.question('Are you sure you want to exit? ', (answer) => {
+    if (answer.match(/^y(es)?$/i)) rl.pause();
+  });
+});
+
 
 /*
  * How does jsdoc work?
@@ -41,4 +51,71 @@ function isRecoverableError(error) {
 // Add some listeners
 process.on('exit', (code) => {
   console.log(`About to exit with code: ${code}`);
+})
+
+// The 'warning' event is emitted whenever Node.js emits a process warning.
+
+// A process warning is similar to an error in that it describes
+// exceptional conditions that are being brought to the user's attention.
+// However, warnings are not part of the normal Node.js and JavaScript
+// error handling flow. Node.js can emit warnings whenever it detects bad
+// coding practices that could lead to sub-optimal application
+// performance, bugs, or security vulnerabilities.
+
+process.on('warning', (warning) => {
+  console.warn(warning.name);    // Print the warning name
+  console.warn(warning.message); // Print the warning message
+  console.warn(warning.stack);   // Print the stack trace
 });
+
+// A common use case for readline is to consume an input file one line at
+// a time. The easiest way to do so is leveraging the fs.ReadStream API as
+// well as a for await...of loop
+// @param {str} fileobj File to read
+// todo: check it exists
+async function processLineByLine(fileobj) {
+  const fileStream = fs.createReadStream(fileobj);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    console.log(`Line from file: ${line}`);
+  }
+}
+
+
+// function read(f) {
+//   return fs.readFileSync(f).toString();
+// }
+
+/*
+ * @param {str} path File path
+ */
+function include(path) {
+  let code = fs.readFileSync(path, 'utf-8');
+  vm.runInThisContext(code, path);
+}
+
+
+// The 'reset' event is emitted when the REPL's context is reset. This
+// occurs whenever the .clear command is received as input unless the REPL
+// is using the default evaluator and the repl.REPLServer instance was
+// created with the useGlobal option set to true. The listener callback will
+// be called with a reference to the context object as the only argument.
+
+// This can be used primarily to re-initialize REPL context to some pre-defined state:
+function initializeContext(context) {
+  context.m = 'test';
+}
+
+// const r = repl.start({prompt: '> '});
+// initializeContext(r.context);
+
+// called with .clear
+// r.on('reset', initializeContext);
