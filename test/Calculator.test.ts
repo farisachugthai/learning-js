11d33cd79c5c257ad313804b3ba67a6ec82671a8
farisachugthai <farischugthai@gmail.com>
2020-08-31 17:59:08 -0400

    Test the repo using CommonJS

--- test/Calculator.test.ts
+++ test/Calculator.test.ts
@@ -1,7 +1,9 @@
-let assert = require("assert");
-import { Calculator, test } from "../src/Calculator";
+// let assert = require("assert");
+import * as assert from "assert";
+import { Calculator, test } from "../src/Calculator.js";
 
 let c = new Calculator();
 test(c, "1+2*33/11="); // prints 9
 
-assert.assertTrue(test(c, "1+2*33/11="), 9)
+// only prints 9 doesn't return the value
+// assert.equal(test(c, "1+2*33/11="), 9)
